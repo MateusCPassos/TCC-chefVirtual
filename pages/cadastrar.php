bdb82ef332abdb1b5412d2fcd5185f31d519b227
Mateus Cardoso Passos <mateuscardoso@hotmail.com>
2024-04-02 15:11:13 -0300

    aterações no cadastro

--- pages/cadastrar.php
+++ pages/cadastrar.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Usuário</title>
+    <link rel="stylesheet" href="../css/cadastro.css">
 </head>
 <body>
-    <form method="post" action="../cadastro.php">
+    <form method="post" action="../cadastro.php" onsubmit="return validarForm()">
         Nome: <input type="text" name="name" required><br>
         E-mail: <input type="email" name="email" required><br>
-        Senha: <input type="password" name="password" required><br> <!-- Adicionado 'required' -->
-        <input type="submit" value="Cadastrar">
+        Senha: <input type="password" id="password" name="password" required><br> 
+        Confirmar Senha: <input type="password" id="confirm_password" required><br> 
+        <button type="submit">Cadastrar</button>
     </form>
+
+    <script>
+        function validarForm() {
+            var password = document.getElementById("password").value;
+            var confirm_password = document.getElementById("confirm_password").value;
+
+            if (password != confirm_password) {
+                alert("As senhas não coincidem!");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
 </body>
 </html>
---
+++ pages/index.php
@@ -0,0 +1,24 @@
+<?php
+  session_start();
+  require_once "../config/conecta.php";
+
+  function logout(){
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+    exit;
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>home</title>
+</head>
+<body>
+  <h1>teste</h1>
+</body>
+</html>

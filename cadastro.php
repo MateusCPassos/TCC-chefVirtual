53f32a38dee95cc5f079705161f5bf11dedbb15f
Mateus Cardoso Passos <mateuscardoso@hotmail.com>
2024-04-15 13:52:20 -0300

    alterações

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once "config/conecta.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -11,27 +10,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(empty($name) || empty($email) || empty($password)){
         echo "Por favor, preencha todos os campos.";
     } else {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        $uuid = uniqid(); // Gera um UUID único
-
-        // Verifica se o usuário já existe
-        $stmt_check = $pdo->prepare("SELECT * FROM usuario WHERE email = ?");
-        $stmt_check->execute([$email]);
-        $existing_user = $stmt_check->fetch(PDO::FETCH_ASSOC);
-        $stmt_check->closeCursor(); 
-        if ($existing_user) {
-            echo "Usuário já existe";
+        // Verifica se a senha tem pelo menos 6 caracteres
+        if (strlen($password) < 6) {
+            echo "A senha deve ter pelo menos 6 caracteres.";
         } else {
-            // Insere o novo usuário com UUID
-            $sql = "INSERT INTO usuario (uuid, name, email, password) VALUES (?, ?, ?, ?)";
-            $stmt = $pdo->prepare($sql);
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            $uuid = uniqid(); // Gera um UUID único
 
-            if ($stmt->execute([$uuid, $name, $email, $hashed_password])) {
-                echo "Usuário criado";
+            // Verifica se o usuário já existe
+            $stmt_check = $pdo->prepare("SELECT * FROM usuario WHERE email = ?");
+            $stmt_check->execute([$email]);
+            $existing_user = $stmt_check->fetch(PDO::FETCH_ASSOC);
+            $stmt_check->closeCursor(); 
+            if ($existing_user) {
+                echo "Este e-mail já está sendo utilizado.";
             } else {
-                echo "Erro ao criar usuário";
+                // Insere o novo usuário com UUID
+                $sql = "INSERT INTO usuario (uuid, name, email, password) VALUES (?, ?, ?, ?)";
+                $stmt = $pdo->prepare($sql);
+
+                if ($stmt->execute([$uuid, $name, $email, $hashed_password])) {
+                    header("Location: pages/index.php");
+                    exit(); 
+                } else {
+                    echo "Erro ao criar usuário.";
+                }
+                $stmt->closeCursor();
             }
-            $stmt->closeCursor();
         }
     }
 }
--- pages/cadastrar.php
+++ pages/cadastrar.php
@@ -2,7 +2,6 @@
     require_once "header.php"
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,24 +9,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Usuário</title>
     <link rel="stylesheet" href="../css/cadastro.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
 </head>
 <body>
     <form method="post" action="../cadastro.php" class="form" onsubmit="return validateForm()">
         <div class="form-group">
-            <label for="name">Nome:</label>
-            <input type="text" id="name" name="name" required>
+            <label for="name" class="input-label">Nome:</label>
+            <div class="input-container">
+                <input type="text" id="name" name="name" required class="input-field" placeholder="ex: João Silva">
+                <i class="fas fa-user input-icon"></i>
+            </div>
         </div>
         <div class="form-group">
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" required>
+            <label for="email" class="input-label">E-mail:</label>
+            <div class="input-container">
+                <input type="email" id="email" name="email" required class="input-field" placeholder="ex: joaosilva@gmail.com">
+                <i class="fas fa-envelope input-icon"></i>
+            </div>
         </div>
         <div class="form-group">
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required>
+            <label for="password" class="input-label">Senha:</label>
+            <div class="input-container">
+                <input type="password" id="password" name="password" required class="input-field" placeholder="ex: Joao1234">
+                <i class="fas fa-lock input-icon"></i>
+            </div>
         </div>
         <div class="form-group">
-            <label for="confirmPassword">Confirmação de Senha:</label>
-            <input type="password" id="confirmPassword" name="confirmPassword" required>
+            <label for="confirmPassword" class="input-label">Confirmação de Senha:</label>
+            <div class="input-container">
+                <input type="password" id="confirmPassword" name="confirmPassword" required class="input-field" placeholder="ex: Joao1234">
+                <i class="fas fa-lock input-icon"></i>
+            </div>
         </div>
         <div class="button">
             <button type="submit">Cadastrar</button>
@@ -38,13 +50,18 @@
         function validateForm() {
             var password = document.getElementById("password").value;
             var confirmPassword = document.getElementById("confirmPassword").value;
+            var email = document.getElementById("email").value;
 
             if (password != confirmPassword) {
                 alert("As senhas não coincidem!");
                 return false;
             }
-            return true;
-        }
+
+            if (password.length < 6) {
+                alert("A senha deve ter pelo menos 6 caracteres!");
+                return false;
+            }
+    }
     </script>
 </body>
 </html>
